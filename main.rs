89cf763c3d356f3be5b677dbb0759e437e18ba67
Rivera Lopez Carlos Omar <158100410+23170306-Rivera-Lopez-Carlos-Omar@users.noreply.github.com>
2025-10-08 16:59:23 +0000

    08/10/2025

---
+++ main.rs
@@ -0,0 +1,59 @@
+use trpl ::StreamExt;
+
+fn main(){
+trpl0::run (async{
+
+
+let values
+
+let iter :: Iter= values.iter().map(|n| n+2);
+//.map (|n| n+2);
+let mut stream = trpl:stream_from_iter()
+
+
+
+let mut filtered = stream.filter(f)
+
+
+while let Some(value) = stream.next().await{ 
+println!("The values was {value}");
+}
+}
+);
+//Print!("Hello, wolrd!");
+
+
+
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let iter = values.iter().map(|n| n * 2);
+        let mut stream = trpl::stream_from_iter(iter);
+
+        while let Some(value) = stream.next().await {
+            println!("The value was: {value}");
+        }
+
+
+
+
+
+
+
+
+
+
+        use trpl::StreamExt;
+
+fn main() {
+    trpl::run(async {
+        let values = 1..101;
+        let iter = values.map(|n| n * 2);
+        let stream = trpl::stream_from_iter(iter);
+
+        let mut filtered =
+            stream.filter(|value| value % 3 == 0 || value % 5 == 0);
+
+        while let Some(value) = filtered.next().await {
+            println!("The value was: {value}");
+        }
+    });
+}
